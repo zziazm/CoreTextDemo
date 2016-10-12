7cf6c22483973b928706ebb36e44c64f96574da6
zhaoming <zziazm@126.com>
2016-10-12 09:00:21 +0800

    add file

---
+++ CoreText/CoreTextDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CoreTextDemo
+//
+//  Created by TangQiao on 13-12-3.
+//  Copyright (c) 2013年 TangQiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CoreText/CoreTextDemo/Controllers/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CoreTextDemo
+//
+//  Created by TangQiao on 13-12-3.
+//  Copyright (c) 2013年 TangQiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
